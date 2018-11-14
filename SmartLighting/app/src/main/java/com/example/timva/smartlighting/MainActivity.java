64a65c81baf61b366f110c91fdfa6d7e25820ccd
Unknown <tvooren1@avans.nl>
2018-11-14 14:01:59 +0100

    fragmentshit verwijderd

--- SmartLighting/app/src/main/java/com/example/timva/smartlighting/MainActivity.java
+++ SmartLighting/app/src/main/java/com/example/timva/smartlighting/MainActivity.java
@@ -3,12 +3,13 @@ package com.example.timva.smartlighting;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.view.MotionEvent;
+import android.widget.CompoundButton;
 import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
     private Switch emulatorSwitch;
     private RecyclerView recycler;
-    private LampFragment lampFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,13 +17,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         emulatorSwitch = findViewById(R.id.EmulatorSwitch);
+        emulatorSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(emulatorSwitch.isChecked()){
+
+                }
+            }
+        });
         recycler = findViewById(R.id.LampRecycler);
-        lampFragment = findViewById(R.id.LampFragment);
     }
 
     @Override
     protected void onResume(){
         super.onResume();
-
     }
 }
